dcd1261f37ed3a1e1f40ec92bf14af2da53dcc00
Belen <ariasmayra975@gmail.com>
2024-05-10 23:21:11 -0400

    ClaseMomento

--- Snake2.java
+++ Snake2.java
@@ -24,3 +24,52 @@ public class Snake2 extends JFrame {
         Snake2 snake1 = new Snake2();
     }
 }
+
+
+
+
+
+
+
+
+	public class Momento extends Thread {
+		
+		private long last = 0;
+		
+		public Momento() {
+			
+		}
+
+		public void run() {
+			while(true) {
+				if((java.lang.System.currentTimeMillis() - last) > frequency) {
+					if(!gameOver) {
+
+                        if(direccion == "RIGHT") {
+                            snake.x = snake.x + widthPoint;
+                            if(snake.x > width) {
+                                snake.x = 0;
+                            }
+                        } else if(direccion == "LEFT") {
+                            snake.x = snake.x - widthPoint;
+                            if(snake.x < 0) {
+                                snake.x = width - widthPoint;
+                            }                        
+                        } else if(direccion == "UP") {
+                            snake.y = snake.y - heightPoint;
+                            if(snake.y < 0) {
+                                snake.y = height;
+                            }                        
+                        } else if(direccion == "DOWN") {
+                            snake.y = snake.y + heightPoint;
+                            if(snake.y > height) {
+                                snake.y = 0;
+                            }                        
+                        }
+				}
+
+			}
+		}
+	}
+
+}
